9f1b75ff74c5bc60a4ada4aa49512d6701a658f4
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2020-04-01 08:42:52 +0200

    deploy to github

---
+++ js/chunk-2d22d746.e71c66ca.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(a,n,t){"use strict";t.r(n);var e=function(){var a=this,n=a.$createElement,t=a._self._c||n;return t("div",{staticClass:"about"},[t("Navbar"),t("b-container",[t("h1",[a._v("This is an about page")])])],1)},s=[],o=t("d178"),u={name:"About",components:{Navbar:o["a"]}},c=u,i=t("2877"),r=Object(i["a"])(c,e,s,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.e71c66ca.js.map
